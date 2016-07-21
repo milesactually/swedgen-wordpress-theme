dac0ad5a442df4c0bc672b8b88f830517896448b
Miles <m@milesactually.com>
2016-07-20 20:46:29 -0700

    Custom template and CSS for blog page

--- front-page.php
+++ front-page.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * The template for displaying all pages.
+ * The template for displaying the landing page.
  *
- * This is the template that displays all pages by default.
+ * This is the template that displays the landing page by default.
  * Please note that this is the WordPress construct of pages
  * and that other 'pages' on your WordPress site may use a
  * different template.
